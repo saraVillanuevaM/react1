0d6f21831c3ea8c77d20005a2b72b15e90229160
Sara Villanueva <sara.villanuevamonago@gmail.com>
2021-03-17 20:35:03 +0100

    Saying Hello world
    Just playing with react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello world! my name is Sara!
         </p>
         <a
           className="App-link"
